634d6e2294e50c62eb36a93788700af71d854285
Thiago Valverde de Souza <thiago.valverde@versata.com>
2017-09-04 00:09:08 -0300

    Bug fix

--- swiss-view/src/app/config.triangular.settings.js
+++ swiss-view/src/app/config.triangular.settings.js
@@ -9,13 +9,13 @@
     function translateConfig(triSettingsProvider, triRouteProvider) {
         var now = new Date();
         // set app name & logo (used in loader, sidemenu, footer, login pages, etc)
-        triSettingsProvider.setName('Vikings');
-        triSettingsProvider.setCopyright('&copy;' + now.getFullYear() + ' Vikings.tech');
+        triSettingsProvider.setName('VS Consulting');
+        triSettingsProvider.setCopyright('&copy;' + now.getFullYear() + ' VS Consulting');
         triSettingsProvider.setLogo('assets/images/logo.png');
         // set current version of app (shown in footer)
         triSettingsProvider.setVersion('1.0.0');
         // set the document title that appears on the browser tab
-        triRouteProvider.setTitle('Vikings');
+        triRouteProvider.setTitle('VS Consulting');
         triRouteProvider.setSeparator('|');
     }
 })();
